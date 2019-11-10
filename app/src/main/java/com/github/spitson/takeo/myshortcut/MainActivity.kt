3c176be38b49908839ee1fa757ed09ced07287b2
Quentin <vFISHv@gmail.com>
2019-11-10 18:00:12 +0800

    androidx

--- app/src/main/java/com/github/spitson/takeo/myshortcut/MainActivity.kt
+++ app/src/main/java/com/github/spitson/takeo/myshortcut/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.spitson.takeo.myshortcut
 
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.content.pm.ShortcutInfo
